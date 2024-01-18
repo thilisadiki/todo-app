4ccd9716ff6f6a7b9aa49ae8d79c4486b962ba4a
Jason <sadikithili@gmail.com>
2024-01-18 16:22:57 +0200

    add saveTask function

--- main.js
+++ main.js
@@ -14,14 +14,26 @@ function addTask() {
             li.appendChild(span);
         }
         inputBox.value = "";
+        saveData();
 }
 
-listContainer = addEventListener("click", function(e){
+listContainer.addEventListener("click", function(e){
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
+        saveData();
     }
     else if(e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
+        saveData();
     }
 
-},false);
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
